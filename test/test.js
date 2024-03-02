41a00a9d98d77f22259ab9f972b679aa1cb454a8
ff6347 <hash@ff6347.email>
2024-03-02 20:44:44 +0100

    test(snapshot): Remove local path from snapshot
    filePath is different on CI

--- test/test.js
+++ test/test.js
@@ -4,5 +4,9 @@ test("all rules snapshot", async (t) => {
 	const FlatESLint = await loadESLint({ useFlatConfig: true });
 	const eslint = new FlatESLint();
 	const results = await eslint.lintFiles(["./test/files/*.js"]);
-	t.snapshot(results);
+	const cleanedResults = results.map((result) => {
+		return { ...result, filePath: result.filePath.split("eslint-config")[1] };
+	})
+
+	t.snapshot(cleanedResults);
 });
