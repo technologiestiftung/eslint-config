00a4f6da802426445e0fa3f1b4cbfeb6ec9e473e
ff6347 <hash@ff6347.email>
2024-03-02 20:47:34 +0100

    test(snapshot): Split filePath at unique section

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ test("all rules snapshot", async (t) => {
 	const eslint = new FlatESLint();
 	const results = await eslint.lintFiles(["./test/files/*.js"]);
 	const cleanedResults = results.map((result) => {
-		return { ...result, filePath: result.filePath.split("eslint-config")[1] };
+		return { ...result, filePath: result.filePath.split("eslint-config/test")[1] };
 	})
 
 	t.snapshot(cleanedResults);
